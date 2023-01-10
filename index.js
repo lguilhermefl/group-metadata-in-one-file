401f62c8fc2e8be5fc2d17c85d0bf08ea16e9e05
luiz <lguilhermefl@gmail.com>
2023-01-09 23:43:19 -0300

    feat: create group metadata function

---
+++ index.js
@@ -0,0 +1,25 @@
+import * as fs from "fs";
+import * as path from "path";
+
+const filesCount = fs.readdirSync("metadata").length;
+const groupedMetadata = [];
+
+function groupMetadata() {
+  for (let i = 1; i <= filesCount; i++) {
+    const fileName = `${i}.json`;
+    const metadataFolderPath = path.resolve("metadata", fileName);
+
+    fs.readFile(metadataFolderPath, "utf-8", (err, data) => {
+      const dataObject = JSON.parse(data);
+      groupedMetadata.push(dataObject);
+      if (groupedMetadata.length == filesCount) {
+        fs.writeFileSync(
+          "metadata.json",
+          JSON.stringify(groupedMetadata, null, 2)
+        );
+      }
+    });
+  }
+}
+
+groupMetadata();
